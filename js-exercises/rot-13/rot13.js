cfea734f706b08fcc04d72f505ac8705e7c572cb
sandeep jaiswar <jaiswarsandeep119@gmail.com>
2021-02-19 14:24:49 +0530

    updating rot13 solution

--- js-exercises/rot-13/rot13.js
+++ js-exercises/rot-13/rot13.js
@@ -1,21 +1,15 @@
 function rot13(args) {
-  const AlphaStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  let result = '';
-  for (const char of args) {
-    if (char.match(/[A-Za-z]/)) {
-      const index = AlphaStr.search(char);
-      let newInd = index + 12;
-      if (newInd+1 > 25) {
-        newInd -= 25;
-      }else{
-        newInd+=1;
-      }
-      result += AlphaStr[newInd];
+  let rotatedString = '';
+  for (const char in args) {
+    if (args[char].charCodeAt(0) > 77 && args[char].charCodeAt(0) < 91) {
+      rotatedString += String.fromCharCode(args[char].charCodeAt(0) + 13 - 90 + 64);
+    } else if (args[char].charCodeAt(0) <= 77 && args[char].charCodeAt(0) > 64) {
+      rotatedString += String.fromCharCode(args[char].charCodeAt(0) + 13);
     } else {
-      result += char;
+      rotatedString += args[char];
     }
   }
-  return result;
+  return rotatedString;
 }
 
 export {
